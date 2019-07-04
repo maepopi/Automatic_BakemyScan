84c95dd19ff391918c63e2c9417a16bcafab8887
Maelys <maelys@opuscope.com>
2019-07-04 15:05:28 +0200

    First bake ok, but not gltf

--- Automatic_Bake.bat
+++ Automatic_Bake.bat
@@ -2,10 +2,20 @@ set blenderPath=Blender\blender.exe
 
 set bakeScriptPath=Blender\2.79\scripts\addons\BakeMyScan\scripts\bakemyscan.py
 
-set inPath=Input\Femme.fbx
+set inPath=Input\Inuit.fbx
 
-set outPath=Output\Femme.obj
+set outPath=Output\Inuit.obj
 
-%blenderPath% -b -P %bakeScriptPath% -- %InPath% %outPath%
+set colorPath=Input\5000_Inuit_Texture_retouche.png
+
+set target=1500
+
+set method=DECIMATE
+
+set resolution=1024
+
+
+
+%blenderPath% -b -P %bakeScriptPath% -- %InPath% %outPath% -M %method% -X %target% -R %resolution% -c %colorPath%
 
 pause
