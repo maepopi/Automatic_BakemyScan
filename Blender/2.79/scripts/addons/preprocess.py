b8e2542a698f7c21379efa2b56ffaa8c1b5c7565
maepopi <35258413+maepopi@users.noreply.github.com>
2020-03-22 15:31:24 +0100

    add self to set scene

--- Blender/2.79/scripts/addons/preprocess.py
+++ Blender/2.79/scripts/addons/preprocess.py
@@ -185,7 +185,7 @@ class ImportToScene():
     def CheckMultiScene(self):
         self.list_scenes = bpy.data.scenes
         if len(self.list_scenes)>1:
-            SetScene()
+            self.SetScene()
 
         else:
             pass
