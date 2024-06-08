1408044ff9c463fbf583bb442a9c9d0059bf5eec
Dr. Jusseaux <35258413+maepopi@users.noreply.github.com>
2024-06-08 18:41:02 +0200

    deleting binary and reorganization

--- GUI_BMS.py
+++ GUI_BMS.py
@@ -20,6 +20,8 @@ class Ui_MainWindow(object):
         MainWindow.resize(874, 856)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+
+        # Title Label
         self.title_label = QLabel(self.centralwidget)
         self.title_label.setObjectName(u"title_label")
         self.title_label.setGeometry(QRect(10, 20, 781, 31))
@@ -30,61 +32,72 @@ class Ui_MainWindow(object):
         self.title_label.setFrameShape(QFrame.Box)
         self.title_label.setFrameShadow(QFrame.Sunken)
         self.title_label.setAlignment(Qt.AlignCenter)
-        self.plugin_input_button = QPushButton(self.centralwidget)
-        self.plugin_input_button.setObjectName(u"plugin_input_button")
-        self.plugin_input_button.setGeometry(QRect(30, 100, 731, 31))
+
+        # Plugin Input Label and Button
         font1 = QFont()
         font1.setFamily(u"Verdana")
         font1.setPointSize(12)
-        self.plugin_input_button.setFont(font1)
+        
         self.plugin_input_label = QLabel(self.centralwidget)
         self.plugin_input_label.setObjectName(u"plugin_input_label")
         self.plugin_input_label.setGeometry(QRect(230, 70, 331, 20))
         self.plugin_input_label.setFont(font1)
         self.plugin_input_label.setAlignment(Qt.AlignCenter)
+
+        self.plugin_input_button = QPushButton(self.centralwidget)
+        self.plugin_input_button.setObjectName(u"plugin_input_button")
+        self.plugin_input_button.setGeometry(QRect(30, 100, 731, 31))
+        self.plugin_input_button.setFont(font1)
+
+        # Blender Input Label and Button
+        self.blender_input_label = QLabel(self.centralwidget)
+        self.blender_input_label.setObjectName(u"blender_input_label")
+        self.blender_input_label.setGeometry(QRect(230, 140, 331, 20))
+        self.blender_input_label.setFont(font1)
+        self.blender_input_label.setAlignment(Qt.AlignCenter)
+        
+        self.blender_input_button = QPushButton(self.centralwidget)
+        self.blender_input_button.setObjectName(u"blender_input_button")
+        self.blender_input_button.setGeometry(QRect(30, 170, 731, 31))
+        self.blender_input_button.setFont(font1)
+        
+        # Object Input Label and Button
         self.object_input_label = QLabel(self.centralwidget)
         self.object_input_label.setObjectName(u"object_input_label")
-        self.object_input_label.setGeometry(QRect(230, 140, 331, 20))
+        self.object_input_label.setGeometry(QRect(230, 210, 331, 20))
         self.object_input_label.setFont(font1)
         self.object_input_label.setAlignment(Qt.AlignCenter)
+
         self.object_input_button = QPushButton(self.centralwidget)
         self.object_input_button.setObjectName(u"object_input_button")
-        self.object_input_button.setGeometry(QRect(30, 170, 731, 31))
+        self.object_input_button.setGeometry(QRect(30, 240, 731, 31))
         self.object_input_button.setFont(font1)
-        self.polygons_label = QLabel(self.centralwidget)
-        self.polygons_label.setObjectName(u"polygons_label")
-        self.polygons_label.setGeometry(QRect(150, 220, 531, 20))
-        self.polygons_label.setFont(font1)
-        self.polygons_label.setAlignment(Qt.AlignCenter)
-        self.polygons_lineEdit = QLineEdit(self.centralwidget)
-        self.polygons_lineEdit.setObjectName(u"polygons_lineEdit")
-        self.polygons_lineEdit.setGeometry(QRect(30, 260, 731, 31))
+
+        # Polygons Label and LineEdit
         font2 = QFont()
         font2.setFamily(u"Verdana")
         font2.setPointSize(10)
         font2.setItalic(True)
+
+        self.polygons_label = QLabel(self.centralwidget)
+        self.polygons_label.setObjectName(u"polygons_label")
+        self.polygons_label.setGeometry(QRect(150, 290, 531, 20))
+        self.polygons_label.setFont(font1)
+        self.polygons_label.setAlignment(Qt.AlignCenter)
+
+        self.polygons_lineEdit = QLineEdit(self.centralwidget)
+        self.polygons_lineEdit.setObjectName(u"polygons_lineEdit")
+        self.polygons_lineEdit.setGeometry(QRect(30, 320, 731, 31))
         self.polygons_lineEdit.setFont(font2)
         self.polygons_lineEdit.setAlignment(Qt.AlignCenter)
+
+        # Diffuse Resolution Label and ComboBox
         self.diffres_label = QLabel(self.centralwidget)
         self.diffres_label.setObjectName(u"diffres_label")
-        self.diffres_label.setGeometry(QRect(140, 320, 531, 20))
+        self.diffres_label.setGeometry(QRect(140, 370, 531, 20))
         self.diffres_label.setFont(font1)
         self.diffres_label.setAlignment(Qt.AlignCenter)
-        self.normres_label = QLabel(self.centralwidget)
-        self.normres_label.setObjectName(u"normres_label")
-        self.normres_label.setGeometry(QRect(140, 420, 531, 20))
-        self.normres_label.setFont(font1)
-        self.normres_label.setAlignment(Qt.AlignCenter)
-        self.decimate_button = QPushButton(self.centralwidget)
-        self.decimate_button.setObjectName(u"decimate_button")
-        self.decimate_button.setGeometry(QRect(30, 550, 721, 31))
-        font3 = QFont()
-        font3.setFamily(u"Verdana")
-        font3.setPointSize(12)
-        font3.setBold(True)
-        font3.setWeight(75)
-        self.decimate_button.setFont(font3)
-        self.decimate_button.setAutoFillBackground(False)
+
         self.diffres_comboBox = QComboBox(self.centralwidget)
         self.diffres_comboBox.addItem("")
         self.diffres_comboBox.addItem("")
@@ -93,8 +106,16 @@ class Ui_MainWindow(object):
         self.diffres_comboBox.addItem("")
         self.diffres_comboBox.addItem("")
         self.diffres_comboBox.setObjectName(u"diffres_comboBox")
-        self.diffres_comboBox.setGeometry(QRect(30, 350, 721, 31))
+        self.diffres_comboBox.setGeometry(QRect(30, 400, 721, 31))
         self.diffres_comboBox.setFont(font2)
+
+        # Normal Resolution Label and ComboBox
+        self.normres_label = QLabel(self.centralwidget)
+        self.normres_label.setObjectName(u"normres_label")
+        self.normres_label.setGeometry(QRect(140, 450, 531, 20))
+        self.normres_label.setFont(font1)
+        self.normres_label.setAlignment(Qt.AlignCenter)
+
         self.normres_comboBox = QComboBox(self.centralwidget)
         self.normres_comboBox.addItem("")
         self.normres_comboBox.addItem("")
@@ -103,29 +124,51 @@ class Ui_MainWindow(object):
         self.normres_comboBox.addItem("")
         self.normres_comboBox.addItem("")
         self.normres_comboBox.setObjectName(u"normres_comboBox")
-        self.normres_comboBox.setGeometry(QRect(30, 450, 721, 31))
+        self.normres_comboBox.setGeometry(QRect(30, 480, 721, 31))
         self.normres_comboBox.setFont(font2)
         self.normres_comboBox.setLayoutDirection(Qt.LeftToRight)
-        self.plainTextEdit = QPlainTextEdit(self.centralwidget)
-        self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.setGeometry(QRect(30, 630, 721, 101))
+
+        # Decimate Button
+        font3 = QFont()
+        font3.setFamily(u"Verdana")
+        font3.setPointSize(12)
+        font3.setBold(True)
+        font3.setWeight(75)
+
+        self.decimate_button = QPushButton(self.centralwidget)
+        self.decimate_button.setObjectName(u"decimate_button")
+        self.decimate_button.setGeometry(QRect(30, 550, 721, 31))
+        self.decimate_button.setFont(font3)
+        self.decimate_button.setAutoFillBackground(False)
+
+        # Console Output Area
+        self.console = QPlainTextEdit(self.centralwidget)
+        self.console.setObjectName(u"console")
+        self.console.setGeometry(QRect(30, 590, 731, 200))
+        self.console.setFont(font2)
+        self.console.setReadOnly(True)
+
+        # Set Central Widget
         MainWindow.setCentralWidget(self.centralwidget)
+
+        # Menubar
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 874, 21))
         self.menuAutomatic_Bakemyscan = QMenu(self.menubar)
         self.menuAutomatic_Bakemyscan.setObjectName(u"menuAutomatic_Bakemyscan")
         MainWindow.setMenuBar(self.menubar)
+
+        # Statusbar
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
+        # Add Menu Action
         self.menubar.addAction(self.menuAutomatic_Bakemyscan.menuAction())
 
         self.retranslateUi(MainWindow)
-
         QMetaObject.connectSlotsByName(MainWindow)
-    # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
@@ -134,6 +177,8 @@ class Ui_MainWindow(object):
         self.plugin_input_label.setText(QCoreApplication.translate("MainWindow", u"Get the plugin global folder below", None))
         self.object_input_label.setText(QCoreApplication.translate("MainWindow", u"Get the object file below", None))
         self.object_input_button.setText(QCoreApplication.translate("MainWindow", u"...", None))
+        self.blender_input_label.setText(QCoreApplication.translate("MainWindow", u"Get your Blender executable", None))
+        self.blender_input_button.setText(QCoreApplication.translate("MainWindow", u"...", None))
         self.polygons_label.setText(QCoreApplication.translate("MainWindow", u"How many polygons do you want for the resulting object?", None))
         self.polygons_lineEdit.setText(QCoreApplication.translate("MainWindow", u"Enter whole number (no decimals)", None))
         self.diffres_label.setText(QCoreApplication.translate("MainWindow", u"What resolution should the diffuse texture be?", None))
@@ -145,15 +190,10 @@ class Ui_MainWindow(object):
         self.diffres_comboBox.setItemText(3, QCoreApplication.translate("MainWindow", u"1024", None))
         self.diffres_comboBox.setItemText(4, QCoreApplication.translate("MainWindow", u"2048", None))
         self.diffres_comboBox.setItemText(5, QCoreApplication.translate("MainWindow", u"4096", None))
-
         self.normres_comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"Choose the resolution", None))
         self.normres_comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"256", None))
         self.normres_comboBox.setItemText(2, QCoreApplication.translate("MainWindow", u"512", None))
         self.normres_comboBox.setItemText(3, QCoreApplication.translate("MainWindow", u"1024", None))
         self.normres_comboBox.setItemText(4, QCoreApplication.translate("MainWindow", u"2048", None))
         self.normres_comboBox.setItemText(5, QCoreApplication.translate("MainWindow", u"4096", None))
-
-        self.normres_comboBox.setCurrentText(QCoreApplication.translate("MainWindow", u"Choose the resolution", None))
         self.menuAutomatic_Bakemyscan.setTitle(QCoreApplication.translate("MainWindow", u"Automatic Bakemyscan", None))
-    # retranslateUi
-
--- Input/RefBatch.bat
+++ Input/RefBatch.bat
@@ -1,190 +1,188 @@
-set blenderPath=%1\..\..\Blender\blender.exe
+@echo off
 
-set bakeScriptPath=%1\..\..\Blender\2.79\scripts\addons\BakeMyScan\scripts\bakemyscan.py
+REM Set paths
+set blenderPath=blender_placeholder
+set bakeScriptPath=bakescript_placeholder
 
-set convertScriptPath=%1\..\..\Blender\2.79\scripts\addons\Object_Reexport.py
+set convertScriptPath=convert_script_placeholder
+set preProcessScriptPath=preprocess_script_placeholder
 
-set multipleTexturesScript=%1\..\..\Blender\2.79\scripts\addons\multipleTextures.py
-
-set preProcessScriptPath=%1\..\..\Blender\2.79\scripts\addons\preprocess.py
-
-
-
+REM Set object name
 for %%I in (%1) do set name=%%~nxI
 
-mkdir %1\..\..\Output\%name%
+REM Create output directory if not exists
+if not exist "%1\..\..\Output\%name%" (
+    mkdir %1\..\..\Output\%name%
+)
 
 set inputFullPath=%1
-
 set outputFullPath=%1\..\..\Output\%name%
 
-REM THIS FOLDER IS ONLY FOR MULTIPLE TEXTURES
+REM Set texture directory
 set txdir=%1\Color
 
-
-if exist "%inputFullPath%\%name%.obj" goto:obj
-if exist "%inputFullPath%\%name%.fbx" goto:fbx
-if exist "%inputFullPath%\%name%.gltf" goto:gltf
-if exist "%inputFullPath%\%name%.glb" goto:glb
-if exist "%inputFullPath%\%name%.stl" goto:stl
-if exist "%inputFullPath%\%name%.ply" goto:ply
-if exist "%inputFullPath%\%name%.3ds" goto:3ds
-if exist "%inputFullPath%\%name%.dae" goto:dae
-
+REM Check for object file extension
+if exist "%inputFullPath%\%name%.obj" goto :obj
+if exist "%inputFullPath%\%name%.fbx" goto :fbx
+if exist "%inputFullPath%\%name%.gltf" goto :gltf
+if exist "%inputFullPath%\%name%.glb" goto :glb
+if exist "%inputFullPath%\%name%.stl" goto :stl
+if exist "%inputFullPath%\%name%.ply" goto :ply
+if exist "%inputFullPath%\%name%.3ds" goto :3ds
+if exist "%inputFullPath%\%name%.dae" goto :dae
 
 :obj
 set object_extension=obj
-goto:object_extension_done
+goto :object_extension_done
 
 :fbx
 set object_extension=fbx
-goto:object_extension_done
+goto :object_extension_done
 
 :gltf
 set object_extension=gltf
-goto:object_extension_done
+goto :object_extension_done
 
 :glb
 set object_extension=glb
-goto:object_extension_done
+goto :object_extension_done
 
 :stl
 set object_extension=stl
-goto:object_extension_done
+goto :object_extension_done
 
 :ply
 set object_extension=ply
-goto:object_extension_done
+goto :object_extension_done
 
 :3ds
 set object_extension=3ds
-goto:object_extension_done
+goto :object_extension_done
 
 :dae
 set object_extension=dae
-goto:object_extension_done
-
+goto :object_extension_done
 
 :object_extension_done
-echo object extension is %object_extension%
+echo Object extension is %object_extension%
 
+REM Check for image file extension
+if exist "%inputFullPath%\%name%_color.jpg" goto :jpg
+if exist "%inputFullPath%\%name%_color.jpeg" goto :jpeg
+if exist "%inputFullPath%\%name%_color.png" goto :png
 
-if exist "%inputFullPath%\%name%_color.jpg" goto:jpg
-if exist "%inputFullPath%\%name%_color.jpeg" goto:jpeg
-if exist "%inputFullPath%\%name%_color.png" goto:png
-
-if exist "%txdir%\" goto:multipletextures
-
+if exist "%txdir%\" goto :multipletextures
 
 :jpg
 set image_extension=jpg
 set multitexture=No
 set texturepath=%inputFullPath%\%name%_color.jpg
-goto:image_extension_done
+goto :image_extension_done
 
 :jpeg
 set image_extension=jpeg
 set multitexture=No
 set texturepath=%inputFullPath%\%name%_color.jpeg
-goto:image_extension_done
+goto :image_extension_done
 
 :png
 set image_extension=png
 set multitexture=No
 set texturepath=%inputFullPath%\%name%_color.png
-goto:image_extension_done
+goto :image_extension_done
 
 :multipletextures
-echo Checking whether there are multitextures
+echo Checking whether there are multiple textures
 set multitexture=Yes
 set image_extension=None
 set texturepath=%txdir%
-goto:done
+goto :done
 
 :image_extension_done
-echo image_extension is %image_extension%
+echo Image extension is %image_extension%
 
 :done
+echo Multitextures detected
 
-echo multitextures detected
-
-
-REM BASIC VARIABLES
-
+REM Set preprocessing variables
 set processInPath=%1\%name%.%object_extension%
-
-
-REM PREPARATION OF THE OBJECT PRIOR TO DECIMATION
-mkdir %1\Preprocess
-
+if not exist "%1\Preprocess" (
+    mkdir %1\Preprocess
+)
 set preprocess_output_path=%1\Preprocess
 
-%blenderPath% -b -P %preProcessScriptPath% -- -ifp %inputFullPath% -pip %processInPath% -n %name% -oe %object_extension% -ie %image_extension% -pop %preprocess_output_path%  -mt %multitexture% -tp %texturepath%
-REM            1  2         3               4           5               6       7                 8            9                          10           11               12
+REM Run Blender for preprocessing
+%blenderPath% -b -P %preProcessScriptPath% -- -ifp %inputFullPath% -pip %processInPath% -n %name% -oe %object_extension% -ie %image_extension% -pop %preprocess_output_path% -mt %multitexture% -tp %texturepath%
 
-
-set object_extension=obj
-
-REM here we have to precise again which is the extension of the copied texture, since at the first check, if the object is gltf, then it can't find the texture and thus its extension.
-if exist "%1\Preprocess\%name%_color.jpg" goto:jpg_preprocess
-if exist "%1\Preprocess\%name%_color.jpeg" goto:jpeg_preprocess
-if exist "%1\Preprocess\%name%_color.png" goto:png_preprocess
+REM Check for preprocessed image file extension
+if exist "%1\Preprocess\%name%_color.jpg" goto :jpg_preprocess
+if exist "%1\Preprocess\%name%_color.jpeg" goto :jpeg_preprocess
+if exist "%1\Preprocess\%name%_color.png" goto :png_preprocess
 
 :jpg_preprocess
 set image_extension=jpg
-goto:image_extension_preprocess_done
+goto :image_extension_preprocess_done
 
 :jpeg_preprocess
 set image_extension=jpeg
-goto:image_extension_preprocess_done
+goto :image_extension_preprocess_done
 
 :png_preprocess
 set image_extension=png
-goto:image_extension_preprocess_done
-
+goto :image_extension_preprocess_done
 
 :image_extension_preprocess_done
-echo image_extension is %image_extension%
+echo Image extension after preprocessing is %image_extension%
 
-
+REM Set paths for decimation
 set inPath=%1\Preprocess\%name%_clean.%object_extension%
 set colorPath=%1\Preprocess\%name%_color.%image_extension%
 set method=DECIMATE
+set diffuse_resolution=512
+set normal_resolution=512
+set target=2000
 
+REM Create output target directory if not exists
+if not exist "%1\..\..\Output\%name%\%target%" (
+    mkdir "%1\..\..\Output\%name%\%target%"
+)
 
-set diffuse_resolution=1024
-set normal_resolution=1024
-set target=1500
-
-mkdir %1\..\..\Output\%name%\%target%
 set outFolder=%1\..\..\Output\%name%\%target%
+set outPath=%outFolder%\%name%.obj
 
-set outPath=%1\..\..\Output\%name%\%target%\%name%.obj
-
-
+REM Run Blender for decimation
 %blenderPath% -b -P %bakeScriptPath% -- %inPath% %outPath% -M %method% -X %target% -R %diffuse_resolution% -c %colorPath%
 
+REM Rename and set new path
+if exist "%outPath%" (
+    ren "%outPath%" %name%_Mesh_%target%.obj
+    set newPath=%outFolder%\%name%_Mesh_%target%.obj
+) else (
+    echo "File %outPath% not found."
+    exit /b
+)
 
-ren %outPath% %name%_Mesh_%target%.obj
+REM Rename texture files
+if exist "%outFolder%\%name%_albedo.jpg" (
+    ren "%outFolder%\%name%_albedo.jpg" %name%_diffuse_%diffuse_resolution%_%target%.jpg
+)
 
-set newPath=%1\..\..\Output\%name%\%target%\%name%_Mesh_%target%.obj
+if exist "%outFolder%\%name%_normal.jpg" (
+    ren "%outFolder%\%name%_normal.jpg" %name%_normal_%normal_resolution%_%target%.jpg
+)
 
+REM Create GLB output directory if not exists
+if not exist "%outputFullPath%\Glb" (
+    mkdir "%outputFullPath%\Glb"
+)
 
-ren %outFolder%\%name%_albedo.jpg %name%_diffuse_%diffuse_resolution%_%target%.jpg
-ren %outFolder%\%name%_normal.jpg %name%_normal_%normal_resolution%_%target%.jpg
-
-mkdir %outputFullPath%\Glb
-
+REM Set paths for export
 set diffusepath=%outputFullPath%\%target%\%name%_diffuse_%diffuse_resolution%_%target%.jpg
 set normalpath=%outputFullPath%\%target%\%name%_normal_%normal_resolution%_%target%.jpg
 set exportpath=%outputFullPath%\Glb
 set exportname=%name%_Mesh_%target%_%diffuse_resolution%-%normal_resolution%
 
-
-
+REM Run Blender for exporting to GLB
 %blenderPath% -b -P %convertScriptPath% -- -np %newPath% -dp %diffusepath% -nop %normalpath% -ep %exportpath% -en %exportname% -dr %diffuse_resolution% -nr %normal_resolution%
 
-echo process done ! You can close the console !
-
-
-
+echo Process done! You can close the console.
